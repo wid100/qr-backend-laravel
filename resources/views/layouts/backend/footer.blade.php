3fba66c670b56f2c28359485872d3afbe10aecc1
monoj kumar <monoj.womenindigital@gmail.com>
2024-04-18 14:35:22 +0600

    update logo

--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- partial:partials/_footer.html -->
 <footer
     class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-    <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://www.nobleui.com" target="_blank">NobleUI</a>.</p>
-    <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+    <p class="text-muted mb-1 mb-md-0">© 2023 <a href="https://www.womenindigital.net/" target="_blank">Women In Digital</a> All Rights Reserved</p>
+    <p class="text-muted">Design And Developed By <a href="https://www.womenindigital.net/" target="_blank">Women In Digital</a> | <a href="https://luminadev.com" target="_blank">Lumina Dev</a></p>
 </footer>
 <!-- partial -->
