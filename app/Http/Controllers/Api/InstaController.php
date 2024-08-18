2e64d50bcadf43e3532b14ad11e5db8bed6f8b2b
monojkumar112 <monoj.womenindigital@gmail.com>
2024-08-18 10:35:11 +0600

    update instagram

--- app/Http/Controllers/Api/InstaController.php
+++ app/Http/Controllers/Api/InstaController.php
@@ -8,22 +8,15 @@ use App\Models\InstaTemplate;
 
 class InstaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // public function allCategory()
+    // {
+    //     $category = InstaCategory::where('status', '1')->get();
+    //     return response()->json($category);
+    // }
+
     public function allCategory()
     {
-        $category = InstaCategory::where('status', '1')->get();
-        return response()->json($category);
-    }
-
-
-
-    public function allTemplate()
-    {
-        $template = InstaTemplate::where('status', '1')->get();
-        return response()->json($template);
+        $categories = InstaCategory::with('instaTemplates')->get();
+        return response()->json($categories);
     }
 }
