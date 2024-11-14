7cb88d92a1b671dd22e6ad81029f253df1554647
monojkumar112 <monoj.womenindigital@gmail.com>
2024-11-14 14:46:19 +0600

    update

--- resources/views/emails/appointment_declined.blade.php
+++ resources/views/emails/appointment_declined.blade.php
@@ -1,27 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Appointment Declined</title>
-</head>
-
-<body>
-    <h1>Appointment Declined</h1>
-    <p>We regret to inform you that your appointment with ID #{{ $appointmentId }} has been declined.</p>
-
-    @if ($declineMessage)
-        <p>Message: {{ $declineMessage }}</p>
-    @endif
-
-    <p>Thank you for your understanding.</p>
-</body>
-
-</html> --}}
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,8 +23,7 @@
                         <tr>
                             <td align="center" style="margin-bottom: 30px; width: 100%">
                                 <div style="padding: 20px 40px 30px 40px; border-radius: 5px">
-                                    <img style="text-align: center; width: 80px"
-                                        src="https://i.postimg.cc/TwqpqP6h/image-81.png" alt="Bits of Dev" />
+
                                     <h1
                                         style="
                         font-family: sans-serif;
@@ -56,124 +31,33 @@
                         margin-bottom: 5px;
                         font-weight: 700;
                         font-size: 20px;
-                        color: #4391cf;
+                        color: #FFB317;
                       ">
-                                        Congratulations
+                                        Appointment Declined
                                     </h1>
-                                    <span
+                                    <p
                                         style="
-                        color: #4391cf;
+                        color: #FFB317;
                         font-size: 12px;
                         font-weight: 500;
+                        line-height: 150%;
                         font-family: sans-serif;
-                      ">Appointment
-                                        ID: {{ $appointment->id }}</span>
+                      ">
+                                        We regret to inform you that your appointment with ID #{{ $appointmentId }} has
+                                        been declined.</p>
+                                        
+                                    @if ($declineMessage)
+                                        <p >Message: {{ $declineMessage }}</p>
+                                    @endif
+
+                                    <p >Thank you for your understanding.</p>
+
+
                                 </div>
                             </td>
                         </tr>
                         <!-- The 50% width on each cell -->
-                        <tr>
-                            <td>
-                                <table
-                                    style="width: 100%; margin-bottom: 25px; border-bottom: 1px solid #D9D9D9; padding-bottom: 20px;"
-                                    border="0" cellpadding="0" cellspacing="0" width="100%">
-                                    <tbody>
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                    Name:
-                                                </p>
-                                            </td>
 
-                                            <td style="text-align: right;padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                    {{ $appointment->first_name }} {{ $appointment->last_name }}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                    Date:
-                                                </p>
-                                            </td>
-
-                                            <td style="text-align: right; padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                    {{ $appointment->date }}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                    Time:
-                                                </p>
-                                            </td>
-
-                                            <td style="text-align: right; padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                    {{ implode(', ', json_decode($appointment->time_slot)) }}
-                                                </p>
-                                            </td>
-                                        </tr>
-
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                    Location:
-                                                </p>
-                                            </td>
-
-                                            <td style="text-align: right; padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                    {{ $appointment->location ?? 'N/A' }}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                   Meeting Link:
-                                                </p>
-                                            </td>
-
-                                            <td style="text-align: right; padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                    {{ $appointment->meeting_link ?? 'N/A' }}
-                                                </p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #898989; padding: 0">
-                                                    Message:
-                                                </p>
-                                            </td>
-
-                                            <td style="text-align: right; padding-bottom: 10px;">
-                                                <p
-                                                    style="margin: 0; font-size: 14px; font-family: sans-serif; color: #555555; padding: 0">
-                                                 {{ $appointment->approval_message ?? 'No message' }}
-                                                </p>
-                                            </td>
-                                        </tr>
-
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
                         <tr>
                             <td align="center">
                                 <a href="https://smartcardgenerator.net/"
@@ -191,5 +75,3 @@
 </body>
 
 </html>
-
-
