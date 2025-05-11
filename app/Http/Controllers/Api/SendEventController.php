58fed410ededfe7f454aaa3368bcd76b4b509ea4
wid100 <helal.womenindigital@gmail.com>
2025-05-11 18:06:16 +0600

    update

--- app/Http/Controllers/Api/SendEventController.php
+++ app/Http/Controllers/Api/SendEventController.php
@@ -19,35 +19,41 @@ class SendEventController extends Controller
             'id' => 1,
             'title' => 'Annual Company Meeting',
             'description' => 'Join us for the annual meeting to discuss company goals and achievements.',
-            'start' => Carbon::parse('2025-02-15 10:00:00')->format('Ymd\THis\Z'), // UTC format
-            'end' => Carbon::parse('2025-02-15 12:00:00')->format('Ymd\THis\Z'),   // UTC format
-            'location' => 'Company HQ, 123 Business Ave, Cityville',
+            'start' => Carbon::parse('2025-05-11 11:38:00')->format('Ymd\THis\Z'), // UTC format
+            'end' => Carbon::parse('2025-05-11 11:55:00')->format('Ymd\THis\Z'),   // UTC format
+            'location' => 'Womenindigital, 123 Business Ave, Cityville',
             'organizer' => 'smaartcard@gmail.com', // Organizer email
-            'attendee' => 'mhshakil06@gmail.com',  // Attendee email
+            'attendee' => 'Nila.cse01@gmail.com',  // Attendee email
         ];
 
 
 
         $icsContent = <<<EOT
-        BEGIN:VCALENDAR
-        VERSION:2.0
-        PRODID:-//Your Company//NONSGML Event//EN
-        METHOD:REQUEST
-        BEGIN:VEVENT
-        UID:event-{$event['id']}@example.com
-        SUMMARY:{$event['title']}
-        DESCRIPTION:{$event['description']}
-        DTSTART:{$event['start']}
-        DTEND:{$event['end']}
-        LOCATION:{$event['location']}
-        ORGANIZER;CN=Organizer:mailto:{$event['organizer']}
-        ATTENDEE;RSVP=TRUE:mailto:{$event['attendee']}
-        STATUS:CONFIRMED
-        SEQUENCE:0
-        TRANSP:OPAQUE
-        END:VEVENT
-        END:VCALENDAR
-        EOT;
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Laravel//EN
+METHOD:REQUEST
+BEGIN:VEVENT
+UID:{$event['id']}@{$_SERVER['HTTP_HOST']}
+SUMMARY:{$event['title']}
+DESCRIPTION:{$event['description']}
+DTSTART:{$event['start']}
+DTEND:{$event['end']}
+LOCATION:{$event['location']}
+ORGANIZER:{$event['organizer']}
+ATTENDEE:{$event['attendee']}
+STATUS:CONFIRMED
+SEQUENCE:0
+TRANSP:OPAQUE
+BEGIN:VALARM
+TRIGGER:-PT10M
+DESCRIPTION:Reminder
+ACTION:DISPLAY
+END:VALARM
+END:VEVENT
+END:VCALENDAR
+EOT;
+
 
 
         Mail::send([], [], function ($message) use ($icsContent, $event) {
@@ -61,56 +67,6 @@ class SendEventController extends Controller
                     ]
                 )
                 ->setBody("Please find the event details attached. We hope to see you there!");
-        });
-
-        dd('Invitation sent successfully!');
-    }
-    public function sendInvitationEmailV1()
-    {
-
-
-
-        $event = [
-            'id' => 1,
-            'title' => 'Annual Company Meeting',
-            'description' => 'Join us for the annual meeting to discuss company goals and achievements.',
-            'start' => Carbon::parse('2025-02-15 10:00:00')->format('Ymd\THis\Z'), // UTC format
-            'end' => Carbon::parse('2025-02-15 12:00:00')->format('Ymd\THis\Z'),   // UTC format
-            'location' => 'Company HQ, 123 Business Ave, Cityville',
-            'organizer' => 'smaartcard@gmail.com', // Organizer email
-            'attendee' => 'mehadi.womenindigital@gmail.com',  // Attendee email
-        ];
-
-
-
-
-
-        $icsContent = <<<EOT
-        BEGIN:VCALENDAR
-        VERSION:2.0
-        PRODID:-//Your Company//NONSGML Event//EN
-        METHOD:REQUEST
-        BEGIN:VEVENT
-        UID:event-{$event['id']}@example.com
-        DTSTAMP:{$event['start']}
-        SUMMARY:{$event['title']}
-        DESCRIPTION:{$event['description']}
-        DTSTART:{$event['start']}
-        DTEND:{$event['end']}
-        LOCATION:{$event['location']}
-        ORGANIZER;CN=Organizer:mailto:{$event['organizer']}
-        ATTENDEE;RSVP=TRUE;CN=Attendee Name:mailto:{$event['attendee']}
-        STATUS:CONFIRMED
-        SEQUENCE:0
-        TRANSP:OPAQUE
-        END:VEVENT
-        END:VCALENDAR
-        EOT;
-
-        Mail::send([], [], function ($message) use ($icsContent, $event) {
-            $message->to($event['attendee'])
-                ->subject("You're Invited: {$event['title']}")
-                ->setBody($icsContent, 'text/calendar; charset=utf-8; method=REQUEST');
         });
 
         dd('Invitation sent successfully!');
