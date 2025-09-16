09a5d6f8c4fdf24c2af71916aa9b5a710f0ff362
monojkumar112 <monoj.womenindigital@gmail.com>
2025-09-16 18:07:21 +0600

    update health card

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -9,42 +9,55 @@ class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'role_id' => '1',
-            'name' => 'Super Admin',
-            'country' => 'Bangladesh',
-            'phone' => '01792892198',
-            'email' => 'helal@admin.com',
-            'password' => bcrypt('11111111'),
-        ]);
-        DB::table('users')->insert([
-            'role_id' => '2',
-            'name' => 'user',
-            'country' => 'Bangladesh',
-            'phone' => '01792892198',
-            'email' => 'helal@user.com',
-            'password' => bcrypt('11111111'),
-        ]);
-        DB::table('users')->insert([
-            'role_id' => '3',
-            'name' => 'Admin',
-            'country' => 'Bangladesh',
-            'phone' => '01792892198',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('11111111'),
-        ]);
-        DB::table('users')->insert([
-            'role_id' => '4',
-            'name' => 'Block User',
-            'country' => 'Bangladesh',
-            'phone' => '01792892198',
-            'email' => 'helal@block.com',
-            'password' => bcrypt('11111111'),
-        ]);
+        // Super Admin
+        DB::table('users')->updateOrInsert(
+            ['email' => 'helal@admin.com'], // check by email
+            [
+                'role_id' => 1,
+                'name' => 'Super Admin',
+                'country' => 'Bangladesh',
+                'phone' => '01792892198',
+                'password' => bcrypt('11111111'),
+            ]
+        );
+
+        // Normal User
+        DB::table('users')->updateOrInsert(
+            ['email' => 'helal@user.com'],
+            [
+                'role_id' => 2,
+                'name' => 'User',
+                'country' => 'Bangladesh',
+                'phone' => '01792892198',
+                'password' => bcrypt('11111111'),
+            ]
+        );
+
+        // Admin
+        DB::table('users')->updateOrInsert(
+            ['email' => 'admin@admin.com'],
+            [
+                'role_id' => 3,
+                'name' => 'Admin',
+                'country' => 'Bangladesh',
+                'phone' => '01792892198',
+                'password' => bcrypt('11111111'),
+            ]
+        );
+
+        // Block User
+        DB::table('users')->updateOrInsert(
+            ['email' => 'helal@block.com'],
+            [
+                'role_id' => 4,
+                'name' => 'Block User',
+                'country' => 'Bangladesh',
+                'phone' => '01792892198',
+                'password' => bcrypt('11111111'),
+            ]
+        );
     }
 }
